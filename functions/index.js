4f12d942db4fbd46acab69fe4002a19ecb8aecec
Douglas Kagai <kagaidouglas@gmail.com>
2022-12-11 07:46:21 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+exports.signInUser = functions.auth.user().onCreate(user => {
+  return admin.firestore().collection('users').doc(user.uid).set(
+    {
+      email: user.email,
+      yoh: [],
+    }
+  )
+});
+
+exports.userDelete = functions.auth.user().onDelete(user =>{
+  const doc = admin.firestore().collection('users').doc(user.uid);
+  return doc.delete();
+
+})
+exports.addRequest = functions.https.onCall((data, context) => {
+  if(!context.auth){
+    throw new functions.https.HttpsError(
+      'unauthenticated',
+      'Fuck you Niccur'
+    );
+
+  }
+  if(data.text.length > 30){
+    throw new functions.https.HttpsError(
+      'invalid-argument',
+      'must not be above 30 characters'
+    );
+  }
+  return admin.firestore().collection('gangstars').add({
+    text: data.text,
+    upvotes: 0,
+  })
+})
